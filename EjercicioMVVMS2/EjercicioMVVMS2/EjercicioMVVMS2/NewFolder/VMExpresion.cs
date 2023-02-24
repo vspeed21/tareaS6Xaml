eb64a5ab336aab9db9dc2b87abd6b676fd4a301d
Victor Miguel Torres <vtorresquintanilla0206@gmail.com>
2023-02-23 20:12:04 -0600

    tarea finalizada

---
+++ EjercicioMVVMS2/EjercicioMVVMS2/EjercicioMVVMS2/NewFolder/VMExpresion.cs
@@ -0,0 +1,103 @@
+﻿using EjercicioMVVMS2.NewFolder2;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SqlTypes;
+using System.Text;
+using Xamarin.Forms;
+
+namespace EjercicioMVVMS2.NewFolder
+{
+    public class VMExpresion : INotifyPropertyChanged
+    {
+        public VMExpresion() {
+            Sumar = new Command(() =>
+            {
+                Expresion ex = new Expresion()
+                {
+                    numero1 = this.numero1,
+                    numero2 = this.numero2,
+                };
+
+                Resultado = ex.suma();
+            });
+            Restar = new Command(() =>
+            {
+                Expresion ex = new Expresion()
+                {
+                    numero1 = this.numero1,
+                    numero2 = this.numero2,
+                };
+
+                Resultado = ex.resta();
+            });
+            Multiplicar = new Command(() =>
+            {
+                Expresion ex = new Expresion()
+                {
+                    numero1 = this.numero1,
+                    numero2 = this.numero2,
+                };
+
+                Resultado = ex.multiplicacion();
+            });
+            Dividir = new Command(() =>
+            {
+                Expresion ex = new Expresion()
+                {
+                    numero1 = this.numero1,
+                    numero2 = this.numero2,
+                };
+
+                Resultado = ex.division();
+            });
+        }  
+
+        double numero1;
+        public double Numero1
+        {
+            get => numero1;
+            set
+            {
+                numero1 = value;
+                var arg = new PropertyChangedEventArgs(nameof(Numero1));
+                PropertyChanged?.Invoke(this, arg);
+            }
+        }
+
+        double numero2;
+        public double Numero2
+        {
+            get => numero2;
+            set
+            {
+                numero2 = value;
+                var arg = new PropertyChangedEventArgs(nameof(Numero2));
+                PropertyChanged?.Invoke(this, arg);
+            }
+        }
+
+        string resultado;
+
+        public string Resultado
+        {
+
+            get => resultado;
+            set
+            {
+
+                resultado = value;
+                var arg = new PropertyChangedEventArgs(nameof(Resultado));
+                PropertyChanged?.Invoke(this, arg);
+
+            }
+
+        }
+
+        public Command Sumar { get; }
+        public Command Restar { get; }
+        public Command Multiplicar { get; }
+        public Command Dividir { get; }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ EjercicioMVVMS2/EjercicioMVVMS2/EjercicioMVVMS2/NewFolder2/Expresion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EjercicioMVVMS2.NewFolder2
+{
+    public class Expresion
+    {
+        public double numero1 { get; set; }
+        public double numero2 { get; set; }
+
+        
+        public string suma() { 
+            return $"La suma de ambos numeros es {numero1 + numero2}"; 
+        }
+
+        public string resta()
+        {
+            return $"La resta de ambos numeros es {numero1 - numero2}";
+        }
+
+        public string multiplicacion()
+        {
+            return $"La multiplicacion de ambos numeros es {numero1 * numero2}";
+        }
+
+        public string division()
+        {
+            return $"La division de ambos numeros es {numero1 / numero2}";
+        }
+
+    }
+}
